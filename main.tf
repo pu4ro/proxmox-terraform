9359fbf09c9efb4747a048069d0157850ea1aced
pu4ro <weondong94@gmail.com>
2025-09-02 18:21:47 +0900

    revert: use hostpci.device per provider schema (device_id unsupported)

--- main.tf
+++ main.tf
@@ -100,7 +100,7 @@ resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
   dynamic "hostpci" {
     for_each = local.pci_devices_effective[count.index] != "" ? [local.pci_devices_effective[count.index]] : []
     content {
-      device_id = hostpci.value
+      device = hostpci.value
       pcie   = var.hostpci_pcie
       rombar = var.hostpci_rombar
     }
